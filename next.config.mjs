78833d25b3773286e7f82a4062976363ab38b54c
Olgierd <olgierdkrzyzaniak@gmail.com>
2024-10-10 15:16:29 +0200

    Add experimental outputFileTracingIncludes configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': ['./content'],
+    },
+  },
+};
+
 export default nextConfig;
